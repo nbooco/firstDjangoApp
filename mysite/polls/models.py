4fb4cb88ae5946f283e18a715cd28f9e1b648e94
Nathan Booco <nbooco@uw.edu>
2017-02-13 21:05:34 -0700

    Migrated db settings and created question and choice models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# A poll question - includes the text and publication date
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+# An answer to a poll question - references the associated question and includes the text and number of votes
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
