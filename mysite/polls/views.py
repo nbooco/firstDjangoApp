249fcac31046858c2cab065c39c723e850a44436
Nathan Booco <nbooco@uw.edu>
2017-02-14 21:54:50 -0700

    Templated index page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
-# View for the main page
+# View for the main page; displays the five most recent questions
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+            'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # View for seeing a specific question and its responses
 def detail(request, question_id):
